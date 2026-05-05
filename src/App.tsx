33716e62913bda524c32e7645088942596d09005
Esneyders <60870722@mail.isil.pe>
2026-05-05 18:52:34 -0500

    V18

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import MainNavbar from './assets/common/MainNavbar'
+import Mainhero from './assets/common/Mainhero'
 
 const App = () => {
 
   return (
     <>
       <MainNavbar/>
+      <Mainhero/>
     </>
   )
 }
