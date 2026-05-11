b4ae39c064b21179108d253bdfc5509239dd5035
Esneyders <60870722@mail.isil.pe>
2026-05-10 22:31:24 -0500

    V56

--- src/assets/home/footer.tsx
+++ src/assets/home/footer.tsx
@@ -1,78 +1,130 @@
 import logofooter from '../img/img1tv.png'
-
+import instagram from '../img/instagram.png'
+import facebook from '../img/facebook.png'
+import X from '../img/X.png'
+import whtapp from '../img/Whtapp.png'
 
 const Footer = () => {
-        return (
-        <footer className="bg-[#858B6F] text-white py-10 mt-20">
-      
+  return (
+    <footer className="bg-[#838B6F] text-white py-10 mt-10">
+
       <div className="max-w-7xl mx-auto px-6 grid grid-cols-1 md:grid-cols-3 gap-10">
-        
+
         {/* Logo */}
         <div>
-          <h1 className="w-70 h-full">
+          <h1 className="flex w-30 h-auto object-cover">
             <img src={logofooter} alt="" />
           </h1>
 
-          <p className="mt-4 text-gray-300">
-            Descubre las mejores bicicletas para aventura,
-            ciudad y montaña.
+          <p className="mt-3 text-white-300">
+            Encuentra el equilibrio perfecto entre sofisticación y adrenalina. Ropa de vestir, equipamiento deportivo y las mejores bicicletas en un solo lugar.
           </p>
+
+          <div className="grid grid-cols-4 gap-4 -pt-8 w-fit  justify-items-center translate-5">
+            <img className="flex- w-10 h-auto  object-cover -translate-x-5" src={instagram} alt="instagram" />
+            <img className="flex w-10 h-auto  object-cover -translate-x-5 " src={facebook} alt="facebook" />
+            <img className="flex w-10 h-auto  object-cover -translate-x-5 " src={X} alt="facebook" />
+            <img className="flex w-10 h-auto  object-cover -translate-x-5 " src={whtapp} alt="facebook" />
+          </div>
         </div>
+        <div className="flex gap-20">
+          <div className=" flex flex-col gap-4 ">
+            <h2 className=" text-2xl font-semibold mb-4 ">
+              Categorias
+            </h2>
 
-        {/* Enlaces */}
-        <div>
-          <h2 className="text-2xl font-semibold mb-4">
-            Enlaces
-          </h2>
+            <ul className="space-y-2 text-gray-300">
+              <li className="hover:text-white transition whitespace-nowrap">
+                Tendencias y colecciones
+              </li>
 
-          <ul className="space-y-2 text-gray-300">
-            <li className="hover:text-white transition">
-              Inicio
-            </li>
+              <li className="hover:text-white transition">
+                Ropas
+              </li>
 
-            <li className="hover:text-white transition">
-              Productos
-            </li>
+              <li className="hover:text-white transition">
+                Zapatos
+              </li>
 
-            <li className="hover:text-white transition">
-              Nosotros
-            </li>
+              <li className="hover:text-white transition">
+                Accesorios
+              </li>
 
-            <li className="hover:text-white transition">
+            </ul>
+          </div>
+
+          <div className=" flex flex-col gap-4  ">
+            <h2 className=" text-2xl font-semibold mb-4  whitespace-nowrap">
+              Ayuda al Cliente
+            </h2>
+
+            <ul className="space-y-2 text-gray-300">
+              <li className="hover:text-white transition">
+                Preguntas Frecuentes
+              </li>
+
+              <li className="hover:text-white transition">
+                Política de Envíos
+              </li>
+
+              <li className="hover:text-white transition">
+                Devoluciones y Cambios
+              </li>
+
+              <li className="hover:text-white transition">
+                Guía de Tallas
+              </li>
+
+              <li className="hover:text-white transition">
+                Términos y Condiciones
+              </li>
+
+              <li className="hover:text-white transition">
+                Política de Privacidad
+              </li>
+
+            </ul>
+          </div>
+
+          <div className=" flex flex-col gap-4 ml-13  ">
+            <h2 className=" text-2xl font-semibold mb-4  ">
               Contacto
-            </li>
-          </ul>
-        </div>
+            </h2>
 
-        {/* Contacto */}
-        <div>
-          <h2 className="text-2xl font-semibold mb-4">
-            Contacto
-          </h2>
+            <ul className="space-y-2 text-gray-300">
+              <li className="hover:text-white transition whitespace-nowrap">
+                ⚲ Av. Principal 123 Ciudad, País
+              </li>
 
-          <p className="text-gray-300">
-            📍 Lima, Perú
-          </p>
+              <li className="hover:text-white transition">
+                🕻 +1 234 567 8900
+              </li>
 
-          <p className="text-gray-300 mt-2">
-            📞 +51 999 999 999
-          </p>
+              <li className="hover:text-white transition">
+                ✉︎ ventas@lunora.com
+              </li>
 
-          <p className="text-gray-300 mt-2">
-            ✉ contacto@velori.com
-          </p>
+              <li className="hover:text-white transition whitespace-nowrap">
+                Horario de Atención: <br />
+                Lun - Vie: 9:00 AM - 8:00 PM <br />
+                Sábados: 10:00 AM - 6:00 PM <br />
+                Domingos: 11:00 AM - 5:00 PM 
+              </li>
+
+            </ul>
+          </div>
         </div>
 
       </div>
 
       {/* Línea inferior */}
-      <div className="border-t border-gray-600 mt-10 pt-5 text-center text-gray-400">
-        © 2026 Velori. Todos los derechos reservados.
+      <div className="border-t border-white-600 mt-10 pt-5 text-center text-white-400">
+        © 2026 Lunora. Todos los derechos reservados.
       </div>
 
     </footer>
   )
 }
 
-    
+
 export default Footer
