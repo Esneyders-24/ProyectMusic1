81479013b49a1d5e2cd3af6b2685286aeb0b8a74
Esneyders <60870722@mail.isil.pe>
2026-05-12 22:49:18 -0500

    V71

--- src/assets/home/ranking.tsx
+++ src/assets/home/ranking.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export interface Ranking {
     id: number;
@@ -13,18 +13,24 @@ const ranking = () => {
 
      const[ListaRanking, setListaRanking] = useState<Ranking[]>([])
 
+     useEffect(() => {
+        leerServicio()
+     }, [])
+
+     const leerServicio = () => {
+
     fetch("https://andersones78x0.alwaysdata.net/categorias.php")
         .then(Response => Response.json())
         .then(datos => {
             console.log(datos)
             setListaRanking(datos)
         })
-
+        }
 
     return (
     <section className="min-h-screen bg-[#F9FAFB]">
         <div className='max-w-7xl mx-auto px-3 py-20 '>
-            <h2 className="text-6xl  flex justify-center items-center gap-6 text-[#858B6F] font-semibold -mt-20 -translate-y-12 ">Ranking</h2>
+            <h2 className="text-6xl  flex justify-center items-center gap-6 text-[#858B6F] font-semibold -mt-20 -translate-y-12 ">Ranking ✰</h2>
             <table className="min-w-full">
                 <thead className='border rounded-xl p-5'>
                     <tr>
@@ -38,13 +44,13 @@ const ranking = () => {
                 </thead>
                     <tbody>
                         {ListaRanking.map(itemRanking => (
-                     <tr>       
-                        <td className="border rounded-xl p-5 text-center">{itemRanking.id}</td>
-                        <td className="border rounded-xl p-5 text-center">{itemRanking.nombre}</td>
-                        <td className="border rounded-xl p-5 text-center">{itemRanking.puntos}</td>
-                        <td className="border rounded-xl p-5 text-center">{itemRanking.compras}</td>
-                        <td className="border rounded-xl p-5 text-center">{itemRanking.nivel}</td>
-                        <td className="border rounded-xl p-5 text-center">{itemRanking.ciudad}</td>
+                     <tr  key={itemRanking.id} className=" hover:bg-green-200 transition duration-300 ">       
+                        <td className="border p-5 text-center">{itemRanking.id}</td>
+                        <td className="border p-5 text-center">{itemRanking.nombre}</td>
+                        <td className="border p-5 text-center">{itemRanking.puntos}</td>
+                        <td className="border p-5 text-center">{itemRanking.compras}</td>
+                        <td className="border p-5 text-center">{itemRanking.nivel}</td>
+                        <td className="border p-5 text-center">{itemRanking.ciudad}</td>
                     </tr>
                 ))}
                 </tbody>
