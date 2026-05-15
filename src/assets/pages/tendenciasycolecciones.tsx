bf842f206f87148dabb25af707a7a5600c21b6ee
Esneyders <60870722@mail.isil.pe>
2026-05-14 23:49:47 -0500

    V89

--- src/assets/pages/tendenciasycolecciones.tsx
+++ src/assets/pages/tendenciasycolecciones.tsx
@@ -1,6 +1,17 @@
+import Headres from "../Components/Headres"
+
+
 const tendenciasycolecciones = () => {
   return (
-    <div>tendenciasycolecciones</div>
+    <>
+    <Headres titulo="Tendencias y colecciones" subtitulo="Salvame Negreira" img="/"/> 
+    <section>
+      <h2>Favoritos tendencias</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos exercitationem accusamus laboriosam eveniet odio modi, praesentium omnis repudiandae deleniti dicta, nisi ipsum voluptatem. Odit quisquam tenetur minus qui vitae assumenda incidunt tempore voluptatum repellat unde facere, repudiandae cum consequuntur. Nulla illo et possimus veritatis corrupti. Porro sequi molestiae molestias eaque? Ipsum illum, quaerat aliquam nulla quod veritatis voluptatibus molestias sapiente doloribus reprehenderit iste numquam aut quibusdam consequatur nihil facilis excepturi blanditiis optio culpa velit, aliquid autem dolorem? Totam, eos magni.</p>
+      <p>Voluptatum, recusandae aliquam? Excepturi eaque debitis itaque ab hic, incidunt porro, modi praesentium recusandae reprehenderit soluta voluptatem id animi eum molestiae doloribus numquam quae vel officiis fugiat repellendus? Molestias cum tempora molestiae voluptate laboriosam itaque. Totam itaque vero iure incidunt, pariatur repellat? Exercitationem nesciunt blanditiis labore dignissimos aliquid? Quidem mollitia esse, autem labore minima, in atque perferendis veritatis facilis cum sed ratione. Quisquam tempora consectetur tenetur aliquid! Adipisci similique eligendi aliquid provident ipsa. Ad similique nemo quaerat dolores laudantium! Harum!</p>
+      <p>Eius, consectetur? Eaque consequatur rerum optio perferendis voluptate sed labore at iste veritatis omnis recusandae, aperiam tenetur molestias eveniet! Eaque, blanditiis error voluptatem enim perferendis a autem accusantium expedita doloribus sed dolorum odit nisi quis, commodi nulla? Veniam quos, nisi laborum quo reiciendis expedita illum voluptas ad illo. Neque nihil, optio illum cumque nulla facere aperiam accusamus reprehenderit pariatur! Nemo dolor necessitatibus porro quo dolores aliquid, iste at! Placeat repellendus doloremque tempore provident eaque corrupti deserunt autem nesciunt omnis perspiciatis?</p>
+    </section>
+    </>
   )
 }
 
