317e92593c4f2a880daa4f35d5c8def838cf0a24
Esneyders <60870722@mail.isil.pe>
2026-05-16 18:29:42 -0500

    S16-1

--- src/assets/Components/Headres.tsx
+++ src/assets/Components/Headres.tsx
@@ -1,15 +1,19 @@
 interface headrespaginas {
         titulo: String;
         subtitulo: String;
-        img: string;        
+        boton: String;
+        img: string;    
+        boton1: String;    
 }
 
-const Headres = ({titulo, subtitulo, img}: headrespaginas) => {
+const Headres = ({titulo, subtitulo, boton, img, boton1 }: headrespaginas) => {
   return (
-    <header className="h-screen bg-cover bg-center" style={{backgroundImage: `url(${img})`}}>
-        <div className='max-w-7xl mx-auto px-3 py-6'>
-            <h1>{titulo}</h1>
-            <p>{subtitulo}</p>
+    <header className="h-screen bg-center w-auto bg-cover bg-no-repeat  " style={{backgroundImage: `url(${img})`}}>
+          <div className="mx-auto px-40 py-40 ">
+            <button className="w-42 h-15 bg-[#858B6F] rounded-3xl text-white font-serif hover:scale-109 transition-all duration-300">{boton}</button>
+            <h1 className="text-white  text-7xl mt-10 font-bold hover:scale-109 transition-all duration-300">{titulo}</h1>
+            <p className="mt-10 text-4xl text-white font-medium hover:scale-109 transition-all duration-300">{subtitulo}</p>
+              <button className="w-40 h-14 bg-[#858B6F] rounded-3xl text-white font-serif hover:bg-gray-400 transition-all duration-300 mt-10">{boton1}</button>
         </div>
     </header>
   )
