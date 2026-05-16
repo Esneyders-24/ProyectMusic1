8e75527e2721a23f6f52fa1ee0dca154827841b0
Esneyders <60870722@mail.isil.pe>
2026-05-16 18:42:52 -0500

    S16-13

--- src/assets/pages/Zapatos.tsx
+++ src/assets/pages/Zapatos.tsx
@@ -1,9 +1,10 @@
 import Headres from "../Components/Headres"
+import img1 from "../img/d2.png"
 
 const Zapatos = () => {
   return (
        <>
-    <Headres titulo="Tendencias y colecciones" subtitulo="Salvame Negreira" img="/"/> 
+    <Headres titulo="Zapatos & Calzado" subtitulo="Desde elegantes tacones hasta cómodos sneakers, encuentra el calzado perfecto para cada ocasión"   boton="Calzado de alta calidad"    img={img1} boton1="Compra ahora"   /> 
     </>
   )
 }
