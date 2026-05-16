7ab73e4b54a4e0bcc87909a638ef07b458b753e1
Esneyders <60870722@mail.isil.pe>
2026-05-16 18:41:31 -0500

    S16-11

--- src/assets/pages/Fitness.tsx
+++ src/assets/pages/Fitness.tsx
@@ -1,9 +1,10 @@
 import Headres from "../Components/Headres"
+import img1 from "../img/ds7.png"
 
 const Fitness = () => {
   return (
     <>
-    <Headres titulo="Tendencias y colecciones" subtitulo="Salvame Negreira" img="/"/> 
+    <Headres titulo="EQUIPOS DE BIENESTAR" subtitulo="Transforma tu espacio con caminadoras de última generación y estaciones de fuerza diseñadas para un entrenamiento profesional en casa"  boton="Explorar tendencias"    img={img1}   boton1="Descubre"/> 
     </>
   )
 }
