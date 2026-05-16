2b3fe78d45d41ad98ec109198e313e87fcef17dc
Esneyders <60870722@mail.isil.pe>
2026-05-16 18:42:07 -0500

    S16-12

--- src/assets/pages/Ropas.tsx
+++ src/assets/pages/Ropas.tsx
@@ -1,9 +1,10 @@
 import Headres from "../Components/Headres"
+import img1 from "../img/d1.png"
 
 const Ropas = () => {
   return (
      <>
-    <Headres titulo="Tendencias y colecciones" subtitulo="Salvame Negreira" img="/"/> 
+    <Headres titulo="MODA DE VANGUARDIA" subtitulo="Sastrería contemporánea y textiles inteligentes que redefinen tu presencia."  boton="Explore the drop"   img={img1} boton1="Shop the collection"/> 
     </>
   )
 }
