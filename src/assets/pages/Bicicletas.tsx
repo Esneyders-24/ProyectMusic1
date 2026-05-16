b13f943d59217653bab52eb3246502f66a966c0c
Esneyders <60870722@mail.isil.pe>
2026-05-16 18:40:47 -0500

    S16-10

--- src/assets/pages/Bicicletas.tsx
+++ src/assets/pages/Bicicletas.tsx
@@ -1,9 +1,10 @@
 import Headres from "../Components/Headres"
+import img1 from "../img/ds8.png"
 
 const BIcicleta = () => {
   return (
     <>
-      <Headres titulo="Tendencias y colecciones" subtitulo="Salvame Negreira" img="/"/> 
+      <Headres titulo="MOVILIDAD VANGUARDISTA" subtitulo="Descubre el equilibrio perfecto entre diseño aerodinámico y confort urbano para dominar las calles con total libertad."    boton="Descubre el futbol"    img={img1} boton1="obtener acceso"/> 
     </>
   )
 }
