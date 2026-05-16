7ba2ef4274bac71265c2932ea4fdc3e03fd9c880
Esneyders <60870722@mail.isil.pe>
2026-05-16 18:39:41 -0500

    S16-9

--- src/assets/pages/AceesorioDeportivo.tsx
+++ src/assets/pages/AceesorioDeportivo.tsx
@@ -1,9 +1,10 @@
 import Headres from "../Components/Headres"
+import img1 from "../img/ds6.png"
 
 const AccesorioDeportivo = () => {
   return (
     <>
-    <Headres titulo="Tendencias y colecciones" subtitulo="Salvame Negreira" img="/"/> 
+    <Headres titulo="ESENCIALES DEPORTIVOS" subtitulo="Complementos de diseño minimalista y máxima funcionalidad que elevan tu rendimiento y aseguran tu comodidad en la rutina diaria."  boton="Ver lanzamiento" img={img1} boton1="Exploramos"   /> 
     </>
   )
 }
