f71f1a2a2a41e96f721a37d07a750ceb062106c7
Esneyders <60870722@mail.isil.pe>
2026-05-16 18:43:48 -0500

    S16-14

--- src/assets/pages/camisetas.tsx
+++ src/assets/pages/camisetas.tsx
@@ -1,9 +1,10 @@
 import Headres from "../Components/Headres"
+import Ing1 from "../img/ds5.png"
 
 const camisetas = () => {
   return (
     <>
-    <Headres titulo="Tendencias y colecciones" subtitulo="Salvame Negreira" img="/"/> 
+    <Headres titulo="JOYAS DE COLECCIÓN" subtitulo="Camisetas icónicas y ediciones limitadas que fusionan la pasión por el fútbol con el estilo urbano más exigente."   boton="Descubre el futbol"  img={Ing1}   boton1="Compra las colecciones" /> 
     </>
   )
 }
