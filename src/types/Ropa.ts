a9bd6f3e6d3434b4413ac9195a720b83fee26572
Esneyders <60870722@mail.isil.pe>
2026-05-18 23:55:15 -0500

    Lunes 18-17

---
+++ src/types/Ropa.ts
@@ -0,0 +1,9 @@
+export interface Ropa {
+    idRopas : number;
+    nombre: string;
+    descripcion: string;
+    precio: number;
+    imagen: string;
+    Categoria: string;
+    Stock: number;
+}
