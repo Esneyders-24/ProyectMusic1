44f8eb0d859949ba7860c7aafcc376bf00fb57c4
Esneyders <60870722@mail.isil.pe>
2026-05-18 20:02:36 -0500

    Lunes18-10

---
+++ src/assets/services/tendenciasYC_services.ts
@@ -0,0 +1,12 @@
+import { CONFIG } from "../../config";
+import type { tendencias_colecciones } from "../../types/TendenciaYC";
+
+const API_URL = `${CONFIG.API_URL}${CONFIG.ENDPOINTS.TENDENCIASYCOLECCIONES}`
+
+export const fectTendenciayc = async (signal?: AbortSignal): Promise<tendencias_colecciones[]> => {
+    const response = await fetch(API_URL, {signal})
+    if(!response.ok){
+        throw new Error(`Error al obtener tendencias y colecciones: ${response.status} ${response.statusText}`);
+    }
+    return response.json()
+}
