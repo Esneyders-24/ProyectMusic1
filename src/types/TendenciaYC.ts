578d7ffb3d72ab28bbe857a5992c59a5e3e9d4b4
Esneyders <60870722@mail.isil.pe>
2026-05-18 20:03:42 -0500

    Lunes18-12

---
+++ src/types/TendenciaYC.ts
@@ -0,0 +1,9 @@
+export interface tendencias_colecciones {
+    idtendenciayC : number;
+    nombre: string;
+    descripcion: string;
+    precio: number;
+    imagen: string;
+    categoria: string;
+    stock: number;
+}
