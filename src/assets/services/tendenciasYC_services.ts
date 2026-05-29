9458d46c9bea7573b6f46135d0ea8e94815c6196
Esneyders <60870722@mail.isil.pe>
2026-05-29 18:36:54 -0500

    Vs29-5

--- src/assets/services/tendenciasYC_services.ts
+++ src/assets/services/tendenciasYC_services.ts
@@ -3,10 +3,35 @@ import type { tendencias_colecciones } from "../../types/TendenciaYC";
 
 const API_URL = `${CONFIG.API_URL}${CONFIG.ENDPOINTS.TENDENCIASYCOLECCIONES}`
 
-export const fectTendenciayc = async (signal?: AbortSignal): Promise<tendencias_colecciones[]> => {
-    const response = await fetch(API_URL, {signal})
+export const fectTendenciayc = async ( idcategoria?: number | string,   signal?: AbortSignal): 
+Promise<tendencias_colecciones[]> => {
+    const url = new URL(API_URL)
+    if(idcategoria){
+       url.searchParams.append("idcategoria", idcategoria.toString())
+    }
+
+    const response = await fetch(url.toString(), {signal})
     if(!response.ok){
-        throw new Error(`Error al obtener tendencias y colecciones: ${response.status} ${response.statusText}`);
+        throw new Error(`Error al obtener productos: ${response.status} ${response.statusText}`)
     }
     return response.json()
 }
+
+export const fetchTendenciaDetalle = async (
+    idtendenciayC?: number | string,
+    signal?: AbortSignal
+): Promise<tendencias_colecciones[]> => {
+    const url = new URL(API_URL)
+
+    if(idtendenciayC){
+        url.searchParams.append("idtendenciayC", idtendenciayC.toString())
+    }
+
+    const response = await fetch(url.toString(), {signal})
+    if(!response.ok){
+        throw new Error(`Error al obtener el producto: ${response.status} ${response.statusText}`)
+    }
+    return response.json()
+}
+
+
