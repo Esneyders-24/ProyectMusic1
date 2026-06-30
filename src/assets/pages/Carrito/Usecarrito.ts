8c3b3942bfc2bd797c619e19470686fa0a1eb20d
Esneyders <60870722@mail.isil.pe>
2026-06-29 23:22:08 -0500

    Lunes29-2026/3

--- src/assets/pages/Carrito/Usecarrito.ts
+++ src/assets/pages/Carrito/Usecarrito.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react"
+import { useCallback, useEffect, useMemo, useState } from "react"
 import type { ItemCarrito } from "../../../types/ItemCarrito"
 
 const getCarrito  = () => {
@@ -31,16 +31,42 @@ export const useCarrito = () => {
     const actualizarCarrito = useCallback((nuevoCarrito: ItemCarrito[]) => {
             localStorage.setItem("carritocompras", JSON.stringify(nuevoCarrito))
             setListaItems(nuevoCarrito)
-            window.dispatchEvent(new Event("carritoActualizado"))
+            window.dispatchEvent(new Event("CarritoActualizado"))
     }, [])
 
 
 
     const vaciarcarrito = useCallback(() => actualizarCarrito([]), [actualizarCarrito])
 
+    const eliminarPila = useCallback((id: number) => {
+        actualizarCarrito(listaItems.filter(item => item.idtendenciayC !== id))
+    },[listaItems, actualizarCarrito])
+
+    const total = useMemo(() =>
+        listaItems.reduce((acc, item) => acc + (item.precio * item.cantidad), 0)
+     ,[listaItems])
+
+     const  IncrementarCantidad = useCallback(( id: number) => {
+        actualizarCarrito(listaItems.map(item =>
+            item.idtendenciayC === id ? {...item, cantidad: item.cantidad + 1 } : item
+        ))
+     }, [listaItems, actualizarCarrito])
+
+      const  dismunirCantidad = useCallback(( id: number) => {
+        actualizarCarrito(listaItems.map(item =>
+            item.idtendenciayC === id && item.cantidad > 1? {...item, cantidad: item.cantidad - 1 } : item
+        ))
+     }, [listaItems, actualizarCarrito])
+     
+     
+
     return {
         listaItems,
         hasListaItems: Array.isArray(listaItems) && listaItems.length>0,
-        vaciarcarrito
+        vaciarcarrito, 
+        eliminarPila,
+        total,
+        dismunirCantidad, 
+        IncrementarCantidad
     }
 }
