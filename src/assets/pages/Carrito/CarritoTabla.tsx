87f7d0f227e03c0ed8a63923226c1553878bd921
Esneyders <60870722@mail.isil.pe>
2026-06-29 23:21:28 -0500

    Lunes29-2026/2

--- src/assets/pages/Carrito/CarritoTabla.tsx
+++ src/assets/pages/Carrito/CarritoTabla.tsx
@@ -1,4 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {useCarrito } from "./Usecarrito"
+import { faTrash } from "@fortawesome/free-solid-svg-icons"
 
 const tableStyles = {
    wrapper: "overflow-hidden rounded-xl border border-slate-200 bg-white shadow-sm",
@@ -10,7 +12,8 @@ const tableStyles = {
    td: "px-6 py-4 text-sm text-slate-700 whitespace-nowrap"
 }
 const CarritoTabla = () => {
-  const {listaItems, hasListaItems, vaciarcarrito} = useCarrito()
+  const {listaItems, hasListaItems, vaciarcarrito, eliminarPila, total, dismunirCantidad, IncrementarCantidad} = useCarrito()
+
   return (
      <>
             {!hasListaItems ? (
@@ -37,17 +40,41 @@ const CarritoTabla = () => {
                           <td className={tableStyles.td}>{item.nombre}</td>
                           <td className={tableStyles.td}> S/ {item.precio}</td>
                           <td className={tableStyles.td}>{item.tallas}</td>
-                          <td className={tableStyles.td}>{item.cantidad}</td>
+                          <td className={tableStyles.td}>
+                            
+                            <div className=" flex items-center gap-3">  
+                                <button 
+                                onClick={() => dismunirCantidad(item.idtendenciayC)}
+                                className="cursor-pointer">-</button>                         
+                                <span>{item.cantidad}</span>
+                                <button
+                                 onClick={() => IncrementarCantidad(item.idtendenciayC)} 
+                                className="cursor-pointer">+</button>
+                                 </div>
+
+                            
+                            </td>
                           <td className={tableStyles.td}> S/ {(item.precio * item.cantidad).toFixed(2)}</td>
+                          <td className={tableStyles.td}>
+                             <button className="cursor-pointer text-red-500 hover:text-red-800"
+                             onClick={ () => eliminarPila(item.idtendenciayC)}>
+                                <FontAwesomeIcon icon={faTrash} />
+                             </button>
+                             </td>
                        </tr>
                       ))}
                   </tbody>
                </table>
              </div>
-          <div className="mt-4 ">
-             <button onClick={vaciarcarrito}
-             className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition-colors cursor-pointer">Vaciar carrito</button>
+          <div className=" flex mt-4 gap-4 ">
+             <div className="w-30 h-10 py-2 bg-green-800 rounded-lg  text-center text-white " >
+                Total: <span className="text-white text-center"> S/{total.toFixed(2)}</span> 
+             </div>
+             <div onClick={vaciarcarrito}
+              className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition-colors cursor-pointer"> Vaciar Carrito
+             </div>
            </div>
+           
                 </>
             )}
         </>
